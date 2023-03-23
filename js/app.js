1a59acccaa2a5350a3dce5d00feef84fa3e0d37e
Masoumeh Rezai <you@example.com>
2023-03-23 05:24:16 +0430

    work on style section

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
-function zoom() {
+let cil = 1;
+function zom() {
+    let w = document.getElementsByTagName("img");
+    if (cil % 2 == 0) {
+        for (let x = 0; x < w.length; x++) {
+            w[x].style.width = "300px"
+
+        }
+    }
+    else {
+        for (let x = 0; x < w.length; x++) {
+            w[x].style.width = "400px"
+
+        }
+    }
+    cil++
+}
+function go() {
+    let g = document.getElementsByTagName("img");
+    for (let y = 0; y < g.length; y++) {
+        g[y].style.border = " 10px solid gold"
+
+    }
 
 }
